5cf8a1a005ea597eda51418b9dc05e37a27f03dd
garavito16 <lex_94@outlook.com>
2022-01-12 06:50:45 -0500

    refactorizando

--- js/index2.js
+++ js/index2.js
@@ -1,19 +1,7 @@
-var cantidadLikes = [1,1,1];
-var texto1 = document.querySelector("#likesNeil");
-var texto2 = document.querySelector("#likesNichole");
-var texto3 = document.querySelector("#likesJim");
-
 function likear(elemento) {
-    if (elemento.id == "btnNeil") {
-        cantidadLikes[0]++;
-        texto1.innerText = cantidadLikes[0] + " like(s)";
-    }
-    else if (elemento.id == "btnNichole") {
-        cantidadLikes[1]++;
-        texto2.innerText = cantidadLikes[1] + " like(s)";
-    }
-    else if (elemento.id == "btnJim") {
-        cantidadLikes[2]++;
-        texto3.innerText = cantidadLikes[2] + " like(s)";
-    }
+    let componente = elemento.parentElement.querySelector('.cantidadLikes').querySelector('span')
+    let valor = componente.innerText;
+    valor = Number(valor);
+    valor++;
+    componente.innerText = valor;
 }
